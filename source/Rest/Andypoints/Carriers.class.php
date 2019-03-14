67a7bd689f7eca85501969c65b6f32cdb78cf632
Andrew Lalis <andrewlalisofficial@gmail.com>
2019-03-14 09:35:08 +0100

    Integrated carrier stuff in here.

--- source/Rest/Andypoints/Carriers.class.php
+++ source/Rest/Andypoints/Carriers.class.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Rest\Endpoints;
+namespace Rest\Andypoints;
 
+use Rest\AndypointTypes\GetRequest;
 use Rest\Endpoint;
-use Rest\EndpointTypes\GetRequest;
 use Rest\Response;
 
 class Carriers extends Endpoint implements GetRequest
@@ -13,7 +13,17 @@ class Carriers extends Endpoint implements GetRequest
         parent::__construct('/carriers');
     }
 
-    public function get(array $args): Response
+    /**
+     * Responds to a GET request to this resource.
+     *
+     * @param array $path_args Any path arguments provided, with the same name as declared by the URI used to construct
+     * the endpoint which implements this interface.
+     * @param array $args A string-indexed list of arguments provided by the client.
+     *
+     * @return Response A response to this request. This contains both a response code, and an array of data to send
+     * back to the client.
+     */
+    public function get(array $path_args, array $args): Response
     {
         $results = $this->fetchCollectionWithQuery("SELECT * FROM carriers");
         $carriers = array_map(function (array $carrier_data): array {
---
+++ source/Rest/Andypoints/Carriers/Carrier.class.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rest\Andypoints\Carriers;
+
+
+use Rest\AndypointTypes\GetRequest;
+use Rest\Endpoint;
+use Rest\Response;
+
+class Carrier extends Endpoint implements GetRequest
+{
+    public function __construct()
+    {
+        parent::__construct('/carriers/{code}');
+    }
+
+    /**
+     * Responds to a GET request to this resource.
+     *
+     * @param array $path_args Any path arguments provided, with the same name as declared by the URI used to construct
+     * the endpoint which implements this interface.
+     * @param array $args A string-indexed list of arguments provided by the client.
+     *
+     * @return Response A response to this request. This contains both a response code, and an array of data to send
+     * back to the client.
+     */
+    public function get(array $path_args, array $args): Response
+    {
+        $code = filter_var($path_args['code'], FILTER_SANITIZE_STRING);
+
+        $statement = $this->getDb()->prepare("SELECT * FROM carriers WHERE carrier_code = :carrier_code;");
+        $statement->bindValue(':carrier_code', $code);
+        $result = $statement->execute();
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+
+        return new Response(
+            200,
+            $row
+        );
+    }
+}
