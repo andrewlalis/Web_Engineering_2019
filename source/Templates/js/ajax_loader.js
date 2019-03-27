35d93ec54c9d0e9ae428766d2c452d8dbac1ecd1
Andrew Lalis <andrewlalisofficial@gmail.com>
2019-03-27 10:53:31 +0100

    Added aggregate statistics

--- source/Templates/js/ajax_loader.js
+++ source/Templates/js/ajax_loader.js
@@ -2,15 +2,20 @@
  * Only once the document is fully-loaded should the madness begin.
  */
 $(document).ready(function () {
+    loadCodeSelectors(); // Load the list of airport and carrier codes for any select elements on the page which need them.
+
+    // Load all data with some initial default ajax requests.
     loadAirports();
     loadCarriers();
-    loadStatisticSelectOptions();
     loadStatistics();
 
+    // Add listeners for when the user clicks to submit new ajax requests.
     $('#airport_submit').click(loadAirports);
     $('#carrier_submit').click(loadCarriers);
     $('#statistics_submit').click(loadStatistics);
+    $('#aggregate_statistics_submit').click(loadAggregateStatistics);
 
+    // Add listeners for when the user enables/disables year and month selection.
     $('#statistics_year_enabled').change(toggleYearEnabled);
     $('#statistics_month_enabled').change(toggleMonthEnabled);
 });
@@ -43,7 +48,7 @@ function loadCollection(base_endpoint_uri, page, limit, container_id, template_i
 function loadAirports() {
     let page = $('#airport_page').val();
     let limit = $('#airport_limit').val();
-    loadCollection('/api/airports', page, limit, '#airports-list', '#airport_entity')
+    loadCollection('/api/airports', page, limit, '#airports_results', '#airport_entity')
 }
 
 /**
@@ -52,38 +57,69 @@ function loadAirports() {
 function loadCarriers() {
     let page = $('#carrier_page').val();
     let limit = $('#carrier_limit').val();
-    loadCollection('/api/carriers', page, limit, '#carriers-list', '#carrier_entity')
+    loadCollection('/api/carriers', page, limit, '#carriers_results', '#carrier_entity')
 }
 
 /**
- * Loads all available options for the statistics filters.
+ * Loads all available options for any available select elements on the page.
  */
-function loadStatisticSelectOptions() {
-    let airport_code_select = $('#statistics_airport_code');
-    let carrier_code_select = $('#statistics_carrier_code');
+function loadCodeSelectors() {
+    let airport_code_selectors = $('.airport_code_selector');
+    let carrier_code_selectors = $('.carrier_code_selector');
 
-    airport_code_select.empty();
-    carrier_code_select.empty();
-    airport_code_select.append($('<option value="None">None</option>'));
-    carrier_code_select.append($('<option value="None">None</option>'));
+    airport_code_selectors.empty();
+    carrier_code_selectors.empty();
 
-    $.ajax('/api/airport_codes')
-        .done(function (data) {
-            data['content'].forEach(function (entity) {
+    airport_code_selectors.each(function (index, element) {
+        let $element = $(element);
+        if ($element.data('optional') === true) {
+            $element.append('<option value="None">None</option>');
+            $element.val('None');
+        }
+    });
+
+    carrier_code_selectors.each(function (index, element) {
+        let $element = $(element);
+        if ($element.data('optional') === true) {
+            $element.append('<option value="None">None</option>');
+            $element.val('None');
+        }
+    });
+
+    $.when(
+        $.ajax('/api/airport_codes'),
+
+        $.ajax('/api/carrier_codes')
+    )
+        .done(function (airport_codes_data, carrier_codes_data) {
+            let airport_codes = airport_codes_data[0]['content'];
+            let carrier_codes = carrier_codes_data[0]['content'];
+
+            airport_codes.forEach(function (entity) {
                 let code = entity['airport_code'];
-                airport_code_select.append('<option value="' + code + '">' + code + '</option>')
+                airport_code_selectors.append('<option value="' + code + '">' + code + '</option>');
             });
-        });
 
-    $.ajax('/api/carrier_codes')
-        .done(function (data) {
-            data['content'].forEach(function (entity) {
+            carrier_codes.forEach(function (entity) {
                 let code = entity['carrier_code'];
-                carrier_code_select.append('<option value="' + code + '">' + code + '</option>')
+                carrier_code_selectors.append('<option value="' + code + '">' + code + '</option>');
             });
+
+            onCodeSelectorLoadingComplete();
         });
 }
 
+/**
+ * What to do when all code selectors have been completely loaded. This is necessary for anything which has a mandatory
+ * option, yet also must fetch some data when the page first loads.
+ */
+function onCodeSelectorLoadingComplete() {
+    loadAggregateStatistics();
+}
+
+/**
+ * Loads statistics data from the three endpoints.
+ */
 function loadStatistics() {
     let page = $('#statistics_page').val();
     let limit = $('#statistics_limit').val();
@@ -111,7 +147,7 @@ function loadStatistics() {
     }
 
     // Prepare the container for the data.
-    let entity_container_element = $('#statistics_list');
+    let entity_container_element = $('#statistics_results');
     entity_container_element.empty();
 
     $.when(
@@ -120,10 +156,6 @@ function loadStatistics() {
         $.ajax('/api/statistics/minutes_delayed' + uri_params)
     )
         .done(function (flights_data, delays_data, minutes_delayed_data) {
-            console.log('All sub-queries done.');
-            console.log(flights_data[0]['content']);
-            console.log(delays_data[0]['content']);
-            console.log(minutes_delayed_data[0]['content']);
             let flights_content = flights_data[0]['content'];
             let delays_content = delays_data[0]['content'];
             let minutes_delayed_content = minutes_delayed_data[0]['content'];
@@ -142,6 +174,28 @@ function loadStatistics() {
                 };
                 entity_container_element.append(template(statistic_object));
             }
+        });
+}
+
+/**
+ * Loads aggregate statistical data.
+ */
+function loadAggregateStatistics() {
+    let airport_code_1 = $('#aggregate_statistics_airport_code_1').val();
+    let airport_code_2 = $('#aggregate_statistics_airport_code_2').val();
+    let carrier_code = $('#aggregate_statistics_carrier_code').val();
+    let uri = '/api/aggregate_carrier_statistics/' + airport_code_1 + '/' + airport_code_2;
+    if (carrier_code !== 'None') {
+        uri += '?carrier_code=' + carrier_code;
+    }
+
+    $.ajax(uri)
+        .done(function (data) {
+            let template_source = $('#aggregate_statistic_entity').html();
+            let template = Handlebars.compile(template_source);
+            let results_container = $('#aggregate_statistics_results');
+            results_container.empty();
+            results_container.append(template(data['content']));
         });
 }
 
